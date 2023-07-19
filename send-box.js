9006ce352c34c2e368a2788852b79e482c2ebd8d
walid-ahmad <86514476+walid-ahmad@users.noreply.github.com>
2023-07-20 02:47:38 +0500

    using video js cdn

--- send-box.js
+++ send-box.js
@@ -3,15 +3,15 @@ const videoElement = document.getElementById("videoSource");
 videoElement.setAttribute("src", videoUrl);
  */
 
-const videoUrlEndpoint =
-  "https://res.cloudinary.com/dnrmkcfii/video/upload/v1686328228/my_videos/mih4i08le29hlq4mvssk.mp4";
-const videoElement = document.getElementById("videoSource");
+// const videoUrlEndpoint =
+//   "https://res.cloudinary.com/dnrmkcfii/video/upload/v1686328228/my_videos/mih4i08le29hlq4mvssk.mp4";
+// const videoElement = document.getElementById("videoSource");
 
-fetch(videoUrlEndpoint)
-  .then((response) => response.text())
-  .then((videoUrl) => {
-    videoElement.setAttribute("src", videoUrl);
-  })
-  .catch((error) => {
-    console.error("Error fetching video URL:", error);
-  });
+// fetch(videoUrlEndpoint)
+//   .then((response) => response.text())
+//   .then((videoUrl) => {
+//     videoElement.setAttribute("src", videoUrl);
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching video URL:", error);
+//   });
