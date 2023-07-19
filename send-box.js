fe9a96998202e3325dd627bb94ca087d395a697c
walid-ahmad <86514476+walid-ahmad@users.noreply.github.com>
2023-07-20 00:58:15 +0500

    using fetch api

--- send-box.js
+++ send-box.js
@@ -1,3 +1,17 @@
-const videoUrl = "https://www.youtube.com/watch?v=c1XoQQViGMI";
+/* const videoUrl = "https://www.youtube.com/watch?v=c1XoQQViGMI";
 const videoElement = document.getElementById("videoSource");
 videoElement.setAttribute("src", videoUrl);
+ */
+
+const videoUrlEndpoint =
+  "https://res.cloudinary.com/dnrmkcfii/video/upload/v1686328228/my_videos/mih4i08le29hlq4mvssk.mp4";
+const videoElement = document.getElementById("videoSource");
+
+fetch(videoUrlEndpoint)
+  .then((response) => response.text())
+  .then((videoUrl) => {
+    videoElement.setAttribute("src", videoUrl);
+  })
+  .catch((error) => {
+    console.error("Error fetching video URL:", error);
+  });
